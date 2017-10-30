a678b6d98aba16c454a8dadfe5aa79cdba7c01d3
Hanhee <parkhanhee89@gmail.com>
2017-10-30 15:05:40 -0400

    Add mqtt npm

---
+++ HaniCraft/Projects.Node/MQTT/main.js
@@ -0,0 +1,19 @@
+
+/*
+ * Reference sites 
+ * - http://genoworks.jp/homehacks/40 
+ * - https://www.npmjs.com/package/homebridge-dyson-fan
+ */
+
+var mqtt = require('mqtt');
+var client = mqtt.connect('mqtt://test.mosquitto.org')
+
+client.on('connect', function(){
+    client.subscribe('presence');
+    client.publish('presence', 'hello mqtt');
+});
+
+client.on('message', function(topic, message){
+    console.log(message.toString());
+    client.end();
+})
